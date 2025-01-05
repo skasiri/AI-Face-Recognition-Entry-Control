e3f25f11c3ed3f291c5488332b20506c47579d21
Saeid Kasiri <s.k.flower@gmail.com>
2025-01-05 14:56:24 +0330

    config_button

--- bottom_frame.py
+++ bottom_frame.py
@@ -8,7 +8,16 @@ import pickle
 
 def create_bottom_frame(parent):
     bottom_frame = ttk.Frame(parent, padding="10", relief="solid")
+
     bottom_label = ttk.Label(bottom_frame, text="Operations", font=("Helvetica", 16))
     bottom_label.pack(pady=10)
 
+    config_button = ttk.Button(bottom_frame, text="Configuration", command=show_config_window)
+    config_button.pack(pady=10)
+
+
+
     return bottom_frame
+
+def show_config_window():
+    pass
